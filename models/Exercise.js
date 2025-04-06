f5b7dd005ff709cd471a9b767b635e781a03f6c0
sayedhamed3-ga <sayedhamed2004@gmail.com>
2025-04-06 20:14:06 +0300

    Added Exercise model

---
+++ models/Exercise.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const exerciseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 300,
+    },
+    muscleGroup: {
+        type: String,
+        required: true,
+    },
+    bodyPart: {
+        type: String,
+        enum: ['Lower Body', 'Upper Body'],
+        required: true,
+    },
+    equipment: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
